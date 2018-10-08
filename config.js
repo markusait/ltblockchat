bbff7fa091398c2b160d7fe04836d56944355282
Hans_Tausend <einund.tausend@gmail.com>
2018-10-09 00:24:33 +0200

    f

--- config.js
+++ config.js
@@ -3,7 +3,6 @@ module.exports = {
      * IP Addresses for each validator node
      */
     peers: [
-        '138.201.93.202'      //digitserver
-        // '137.248.58.146'    //winkel
+      // ip1 , 1p2
     ]
 }

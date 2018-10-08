7a3deafe2e1cd355d37f1e54595e901122977652
Hans_Tausend <einund.tausend@gmail.com>
2018-10-08 19:24:54 +0200

    monopl

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ module.exports = {
      * IP Addresses for each validator node
      */
     peers: [
-        '137.248.58.146'    //winkel
-        // '138.201.93.202'      //digitserver
+        //'137.248.58.146'    //winkel
+         '138.201.93.202'      //digitserver
     ]
 }
